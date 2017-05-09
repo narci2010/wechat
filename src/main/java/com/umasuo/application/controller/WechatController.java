7eb8c44996e3b685bd609687116d12790e1393d6
umasuo <liuquan89@gmail.com>
2017-05-09 18:42:40 +0800

    update

--- src/main/java/com/umasuo/application/controller/WechatController.java
+++ src/main/java/com/umasuo/application/controller/WechatController.java
@@ -5,6 +5,7 @@ import com.umasuo.infrastructure.config.ApplicationConfig;
 import com.umasuo.infrastructure.utils.ByteUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,21 +16,37 @@ import java.util.Arrays;
  */
 public class WechatController {
 
+  /**
+   * LOG.
+   */
   private static Logger LOG = LoggerFactory.getLogger(WechatController.class);
 
+  /**
+   * application config.
+   */
+  @Autowired
   private transient ApplicationConfig applicationConfig;
 
+  /**
+   * Verify api for wechat.
+   *
+   * @param timestamp String
+   * @param nonce     String
+   * @param signature String
+   * @param echoStr   String
+   * @return String
+   */
   public String get(
       @RequestParam(value = Router.TIMESTAMP) String timestamp,
       @RequestParam(value = Router.NONCE) String nonce,
       @RequestParam(value = Router.SIGNATURE) String signature,
       @RequestParam(value = Router.ECHO_STR) String echoStr) {
-    if (hasNoText(timestamp, nonce, signature, echoStr)) {
-      LOG.error(
-          "wechat api verify failed. Invalid parameters: timestamp {}, nonce {}, signature {}, " +
-              "echostr {}",
-          timestamp, nonce, signature, echoStr);
-      return "";
+    LOG.info("Enter. timestamp: {}, nonce: {}, signature: {}, echostr: {}",
+        timestamp, nonce, signature, echoStr);
+
+    String result = "";
+    if (hasEmpty(timestamp, nonce, signature, echoStr)) {
+      LOG.warn("Verify failed for some parameter has no text. ");
     }
     try {
       String[] arr = new String[] {timestamp, nonce, applicationConfig.getWechatToken()};
@@ -37,22 +54,25 @@ public class WechatController {
       String rawValue = arr[0] + arr[1] + arr[2];
       String verifyString = ByteUtils.getHexString(rawValue);
       if (signature.equals(verifyString.toLowerCase())) {
-        LOG.info(
-            "wechat api verify success. timestamp:{},nonce:{},signature:{},echostr:{}," +
-                "verifyString:{}",
-            timestamp, nonce, signature, echoStr, verifyString);
-        return echoStr;
+        LOG.info("Verify success.");
+        result = echoStr;
       }
     } catch (Exception e) {
       LOG.error(e.getMessage(), e);
     }
     LOG.error(
-        "wechat api verify failed. timestamp {}, nonce {}, signature {}, echostr {}, token {}",
+        "Verify failed.",
         timestamp, nonce, signature, echoStr, applicationConfig.getWechatToken());
     return "";
   }
 
-  private boolean hasNoText(CharSequence... strs) {
+  /**
+   * make sure the char sequence has text.
+   *
+   * @param strs charsequence
+   * @return boolean
+   */
+  private boolean hasEmpty(CharSequence... strs) {
     for (CharSequence str : strs) {
       if (!StringUtils.hasText(str)) {
         return true;

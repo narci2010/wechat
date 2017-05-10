eef00df56a79990f233380189bd6fb26556fa124
umasuo <liuquan89@gmail.com>
2017-05-10 10:54:13 +0800

    add api

--- src/main/java/com/umasuo/application/controller/WechatController.java
+++ src/main/java/com/umasuo/application/controller/WechatController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -36,6 +37,7 @@ public class WechatController {
    * @param echoStr   String
    * @return String
    */
+  @GetMapping(Router.ROOT)
   public String get(
       @RequestParam(value = Router.TIMESTAMP) String timestamp,
       @RequestParam(value = Router.NONCE) String nonce,
